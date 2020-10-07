d7c0150b53e42b0d3a150f39446bdb57706f4726
song-i <rkdlem1613@gmail.com>
2020-10-07 17:12:30 +0900

    10.07_03
    modify build.gradle

--- app/src/main/java/com/example/test/VideoChatActivity.java
+++ app/src/main/java/com/example/test/VideoChatActivity.java
@@ -9,7 +9,13 @@ import us.zoom.sdk.MeetingStatus;
 import us.zoom.sdk.ZoomSDK;
 import us.zoom.sdk.ZoomAuthenticationError;
 import us.zoom.sdk.ZoomSDKAuthenticationListener;
+import us.zoom.sdk.ZoomSDKInitParams;
+import us.zoom.sdk.ZoomSDKInitializeListener;
+
+import android.content.Context;
 import android.os.Bundle;
+
+import static java.util.Calendar.getInstance;
 
 public class VideoChatActivity extends AppCompatActivity {
 
@@ -19,7 +25,24 @@ public class VideoChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video_chat);
     }
 
-    private void initSdk() {
+    private void initSdk(Context context) {
 
+        ZoomSDK sdk = ZoomSDK.getInstance();
+
+        ZoomSDKInitParams params = new ZoomSDKInitParams();
+        params.appKey = Credentials.SDK_KEY;
+        params.appSecret = Credentials.SDK_SECRET;
+        params.domain = Credentials.SDK_DOMAIN;
+        params.enableLog = true;
+
+        ZoomSDKInitializeListener listener = new ZoomSDKInitializeListener() {
+            @Override
+            public void onZoomSDKInitializeResult(int errorCode, int internalErrorCode) { }
+
+            @Override
+            public void onZoomAuthIdentityExpired() { }
+        };
+
+        sdk.initialize(context, listener, params);
     }
 }
