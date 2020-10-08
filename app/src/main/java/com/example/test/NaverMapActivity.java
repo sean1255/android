3a22caf44e97946710c1cb16560f1a72a77beeca
goodday <sean1255@naver.com>
2020-10-09 04:18:12 +0900

    map
    마크표시, 코드 정리

--- app/src/main/java/com/example/test/NaverMapActivity.java
+++ app/src/main/java/com/example/test/NaverMapActivity.java
@@ -1,8 +1,11 @@
 package com.example.test;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.InputStreamReader;
@@ -12,9 +15,15 @@ import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.util.FusedLocationSource;
 
 import java.net.URI;
@@ -22,15 +31,134 @@ import java.net.URI;
 public class NaverMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
-    private FusedLocationSource locationSource;
+    private FusedLocationSource locationSource;   // 현재 위치 표시
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_navermap);
+        setContentView(R.layout.activity_navermap);
 
         MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
         mapFragment.getMapAsync(this);
+    }
+
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        // ...
+        Marker marker1 = new Marker();
+        marker1.setPosition(new LatLng(35.119836, 129.081171));
+        marker1.setMap(naverMap);
+
+        Marker marker2 = new Marker();
+        marker2.setPosition(new LatLng(35.159516, 129.068027));
+        marker2.setMap(naverMap);
+
+        Marker marker3 = new Marker();
+        marker3.setPosition(new LatLng(35.137958, 129.064754));
+        marker3.setMap(naverMap);
+
+        Marker marker4 = new Marker();
+        marker4.setPosition(new LatLng(35.1198471,129.062608));
+        marker4.setMap(naverMap);
+
+        Marker marker5 = new Marker();
+        marker5.setPosition(new LatLng(35.1172166,129.0521816));
+        marker5.setMap(naverMap);
+
+        Marker marker6 = new Marker();
+        marker6.setPosition(new LatLng(35.154554,129.035846));
+        marker6.setMap(naverMap);
+
+        Marker marker7 = new Marker();
+        marker7.setPosition(new LatLng(35.104241, 128.981231));
+        marker7.setMap(naverMap);
+
+        Marker marker8 = new Marker();
+        marker8.setPosition(new LatLng(35.097060, 128.960205));
+        marker8.setMap(naverMap);
+
+        Marker marker9 = new Marker();
+        marker9.setPosition(new LatLng(35.094554, 128.983375));
+        marker9.setMap(naverMap);
+
+        Marker marker10 = new Marker();
+        marker10.setPosition(new LatLng(35.1524071,128.9666643));
+        marker10.setMap(naverMap);
+
+        Marker marker11 = new Marker();
+        marker11.setPosition(new LatLng(35.194966, 129.080782));
+        marker11.setMap(naverMap);
+
+        Marker marker12 = new Marker();
+        marker12.setPosition(new LatLng(35.192106, 129.082179));
+        marker12.setMap(naverMap);
+
+        Marker marker13 = new Marker();
+        marker13.setPosition(new LatLng(35.212824, 129.081757));
+        marker13.setMap(naverMap);
+
+        Marker marker14 = new Marker();
+        marker14.setPosition(new LatLng(35.203356, 129.070226));
+        marker14.setMap(naverMap);
+
+        Marker marker15 = new Marker();
+        marker15.setPosition(new LatLng(35.200532, 129.056069));
+        marker15.setMap(naverMap);
+
+        Marker marker16 = new Marker();
+        marker16.setPosition(new LatLng(35.218216, 129.037633));
+        marker16.setMap(naverMap);
+
+        Marker marker17 = new Marker();
+        marker17.setPosition(new LatLng(35.195476, 129.008505));
+        marker17.setMap(naverMap);
+
+
+
+//        InfoWindow infoWindow = new InfoWindow();
+//        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
+//            @NonNull
+        //           @Override
+        //           public CharSequence getText(@NonNull InfoWindow infoWindow1) {
+//                return "복지관";
+//            }
+//        });
+
+        locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
+        naverMap.setLocationSource(locationSource);
+        UiSettings uiSettings = naverMap.getUiSettings();  // 현재 위치 ui버튼
+        uiSettings.setLocationButtonEnabled(true);
+
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow); // None, NoFollow, Follow, Face 가 있음
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {  // 위치 권한 허용 시도
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        switch (requestCode) {
+            case ACCESS_LOCATION_PERMISSION_REQUEST_CODE:
+                locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                return;
+
+//            if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+//                //if (!locationSource.isActivated()) {  // 권한 거부됨
+            //    naverMap.setLocationTrackingMode(LocationTrackingMode);
+//                return;
+//            }
+        }
+    }
+}
+
+
+
 
 //        class ApiExplorer {
 //            public void main(String[] args) throws IOException {
@@ -58,27 +186,7 @@ public class NaverMapActivity extends AppCompatActivity implements OnMapReadyCal
 //               }
 //                rd.close();
 //                conn.disconnect();
- //              System.out.println(sb.toString());
- //          }
-        }
+//              System.out.println(sb.toString());
+//          }
+//       }
 //}
-
-    @Override
-    public void onMapReady(@NonNull com.naver.maps.map.NaverMap naverMap) {
-        locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
-        naverMap.setLocationSource(locationSource);
-        UiSettings uiSettings = naverMap.getUiSettings();
-        uiSettings.setLocationButtonEnabled(true);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case ACCESS_LOCATION_PERMISSION_REQUEST_CODE:
-                locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                return;
-        }
-    }
-}
