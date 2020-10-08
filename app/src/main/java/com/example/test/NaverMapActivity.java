28430e03b5f12491f68524688e3ff68eb2b7e7f5
goodday <sean1255@naver.com>
2020-10-09 00:06:39 +0900

    기존 코드에서 필요없는 부분 제거, 앱상단바 제거

--- app/src/main/java/com/example/test/NaverMapActivity.java
+++ app/src/main/java/com/example/test/NaverMapActivity.java
@@ -20,47 +20,48 @@ import com.naver.maps.map.util.FusedLocationSource;
 import java.net.URI;
 
 public class NaverMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
     private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
     private FusedLocationSource locationSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navermap);
+       setContentView(R.layout.activity_navermap);
 
         MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        class ApiExplorer {
-            public void main(String[] args) throws IOException {
-                StringBuilder urlBuilder = new StringBuilder("http://open.gyeongnam.go.kr/rest/서비스키"); /*URL*/
-                urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=서비스키"); /*Service Key*/
-                urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + URLEncoder.encode("서비스키", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
-                urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-                urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-                urlBuilder.append("&" + URLEncoder.encode("title", "UTF-8") + "=" + URLEncoder.encode("소원의항구", "UTF-8")); /*기관명*/
-                URL url = new URL(urlBuilder.toString());
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Content-type", "application/json");
-                System.out.println("Response code: " + conn.getResponseCode());
-                BufferedReader rd;
-                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                } else {
-                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-                }
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = rd.readLine()) != null) {
-                    sb.append(line);
-                }
-                rd.close();
-                conn.disconnect();
-                System.out.println(sb.toString());
-            }
+//        class ApiExplorer {
+//            public void main(String[] args) throws IOException {
+//                StringBuilder urlBuilder = new StringBuilder("http://open.gyeongnam.go.kr/rest/서비스키"); /*URL*/
+//                urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=서비스키"); /*Service Key*/
+//                urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + URLEncoder.encode("서비스키", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
+//                urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+//                urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+//                urlBuilder.append("&" + URLEncoder.encode("title", "UTF-8") + "=" + URLEncoder.encode("소원의항구", "UTF-8")); /*기관명*/
+//                URL url = new URL(urlBuilder.toString());
+//               HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                conn.setRequestMethod("GET");
+//                conn.setRequestProperty("Content-type", "application/json");
+//                System.out.println("Response code: " + conn.getResponseCode());
+//                BufferedReader rd;
+//                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+//                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//                } else {
+//                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+//                }
+//               StringBuilder sb = new StringBuilder();
+//               String line;
+//               while ((line = rd.readLine()) != null) {
+//                   sb.append(line);
+//               }
+//                rd.close();
+//                conn.disconnect();
+ //              System.out.println(sb.toString());
+ //          }
         }
-    }
+//}
 
     @Override
     public void onMapReady(@NonNull com.naver.maps.map.NaverMap naverMap) {
