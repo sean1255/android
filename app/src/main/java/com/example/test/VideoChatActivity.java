25f78287a4322f220cca191b9417ae1c30f1c9d0
song-i <rkdlem1613@gmail.com>
2020-10-08 11:09:00 +0900

    10.08_02
    fix dialog

--- app/src/main/java/com/example/test/VideoChatActivity.java
+++ app/src/main/java/com/example/test/VideoChatActivity.java
@@ -1,16 +1,21 @@
 package com.example.test;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import us.zoom.sdk.JoinMeetingOptions;
+import us.zoom.sdk.JoinMeetingParams;
 import us.zoom.sdk.MeetingError;
 import us.zoom.sdk.MeetingService;
 import us.zoom.sdk.MeetingServiceListener;
 import us.zoom.sdk.MeetingStatus;
+import us.zoom.sdk.ZoomApiError;
 import us.zoom.sdk.ZoomSDK;
 import us.zoom.sdk.ZoomAuthenticationError;
 import us.zoom.sdk.ZoomSDKAuthenticationListener;
 import us.zoom.sdk.ZoomSDKInitParams;
 import us.zoom.sdk.ZoomSDKInitializeListener;
+import us.zoom.sdk.StartMeetingOptions;
 
 import android.content.Context;
 import android.graphics.Paint;
@@ -23,20 +28,24 @@ import static java.util.Calendar.getInstance;
 
 public class VideoChatActivity extends AppCompatActivity {
 
+    /*
     private JoinMeetingDialog joinMeetingDialog;
     private LoginDialog loginDialog;
     private Button join;
     private Button login;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_chat);
 
+        /*
         //setting
         join = (Button)findViewById(R.id.join_button);
         login = (Button)findViewById(R.id.login_button);
 
+        // create a dialog to join meeting
         join.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -45,6 +54,7 @@ public class VideoChatActivity extends AppCompatActivity {
             }
         });
 
+        // create a dialog to login
         login.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -52,8 +62,11 @@ public class VideoChatActivity extends AppCompatActivity {
                 loginDialog.show();
             }
         });
+
+        */
     }
 
+    /*
     private View.OnClickListener joinMeetingListener = new View.OnClickListener() {
         public void onClick(View v) {
             Toast.makeText(getApplicationContext(), "화상 채팅을 시작합니다.", Toast.LENGTH_SHORT).show();
@@ -82,6 +95,8 @@ public class VideoChatActivity extends AppCompatActivity {
         }
     };
 
+    */
+
     private void initSdk(Context context) {
 
         ZoomSDK sdk = ZoomSDK.getInstance();
@@ -102,4 +117,41 @@ public class VideoChatActivity extends AppCompatActivity {
 
         sdk.initialize(context, listener, params);
     }
+
+    // 1. the join Meeting function
+    private void joinMeeting(Context context, String meetingNumber, String password) {
+        MeetingService meetingService = ZoomSDK.getInstance().getMeetingService();
+        JoinMeetingOptions options = new JoinMeetingOptions();
+        JoinMeetingParams params = new JoinMeetingParams();
+        params.displayName = "";
+        params.meetingNo = meetingNumber;
+        params.password = password;
+        meetingService.joinMeetingWithParams(context, params, options);
+    }
+    // 2-1. the login function
+    private void login(String username, String password) {
+        int result = ZoomSDK.getInstance().loginWithZoom(username, password);
+        if (result == ZoomApiError.ZOOM_API_ERROR_SUCCESS) {
+
+            // 2-2. After request is executed, listen for the authentication result prior to starting a meeting
+            ZoomSDK.getInstance().addAuthenticationListener(authLsitener);
+        }
+    }
+
+    // 2-3. the start Meeting function
+    private void startMeeting(Context context) {
+        ZoomSDK sdk = ZoomSDK.getInstance();
+        if(sdk.isLoggedIn()) {
+            MeetingService meetingService = sdk.getMeetingService();
+            StartMeetingOptions options = new StartMeetingOptions();
+            meetingService.startInstantMeeting(context, options);
+        }
+    }
+
+    // create a dialog to join meeting
+    private void createJoinMeetingDialog() {
+        new
+                AlertDialog.Builder(this).setView(R.layout.activity_join_meeting_dialog).setPositiveButton("JOIN")
+    }
+
 }
